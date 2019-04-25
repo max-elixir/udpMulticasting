990a21497a553babc9600175c19763528f8833a0
Andrew Rodriguez <andrew.m.rodriguez.000@gmail.com>
2019-04-24 19:19:24 -0500

    attempt to fix recovery of two dropped messages in a row

--- client.c
+++ client.c
@@ -13,11 +13,27 @@
 #include <errno.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <netinet/tcp.h>
+#include <netinet/in.h>
+#include <sys/signal.h>
+
+typedef struct request {
+    struct request * prevReq;
+    struct request * nextReq;
+    char thisReq[10];
+} request;
+
+request * lastReq = NULL;
+request * oldReq = NULL;
 
 int nbytes, flags;
 struct sockaddr_in beacon;
 
-int requestDropped(void *);
+void insert(char * msg);
+int length();
+void removeTail();
+void printList();
+int requestDropped();
 
 int main(int argc, char *argv[]){
     int sock, i, size;    
@@ -74,7 +90,8 @@ int main(int argc, char *argv[]){
     int addrlen, sentNum, sent, size2, nbytes2, prev=0, caught;
     char *token;
     char * copy = (char *) malloc(99);
-    char * delimit = "---"; 
+    char * delimit = "---";
+    pthread_create(&reqThread, NULL, (void *)requestDropped,NULL);
     while(1){
         addrlen = sizeof(beacon);
         nbytes = 99;
@@ -88,8 +105,7 @@ int main(int argc, char *argv[]){
 		prev++;
                 char request[5];
                 sprintf(request, "%d", prev);
-		pthread_create(&reqThread, NULL, (void *)requestDropped, (void *)request);	
-		
+                insert((char*)&request);	
 	}else{	
 		strcpy(copy, message);
         	strtok(message, delimit);
@@ -105,11 +121,11 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
-int requestDropped(void *requestMessage){
-		int nbytes2, sock2, connected, sent, caught;
+int requestDropped(){
+		int nbytes2, sock2, connected, sent, caught, yes;
 		struct sockaddr_in target_pc, me;
-		char *request= (char *)malloc(5);
-		request = (char *)requestMessage;
+		char *request;
+		request = (char*)lastReq->thisReq;
 		nbytes2= 5;
 		char *message2 = (char *) malloc(99);
 		/* Establish TCP connection to Beacon*/
@@ -130,6 +146,14 @@ int requestDropped(void *requestMessage){
     		me.sin_port = htons(0);
     		me.sin_addr.s_addr = htonl(INADDR_ANY);	
 
+                // Enable keepalive
+                setsockopt(sock2, SOL_SOCKET, SO_KEEPALIVE, &yes, sizeof(yes));
+                setsockopt(sock2, IPPROTO_TCP, TCP_KEEPIDLE, &yes, sizeof(yes));
+                setsockopt(sock2, IPPROTO_TCP, TCP_KEEPINTVL, &yes, sizeof(yes));
+                yes = 10;
+                setsockopt(sock2, IPPROTO_TCP, TCP_KEEPCNT, &yes, sizeof(yes));
+
+
 		/* Attempt to connect to the TCP server  */
     		connected = connect(sock2, (struct sockaddr *) &target_pc, 
 			sizeof(target_pc));
@@ -137,23 +161,60 @@ int requestDropped(void *requestMessage){
         		printf("Failed to connect: %d\n errno = %s\n", connected, strerror(errno));
         		return -1;
     		}	
-		sent = send(sock2, request, nbytes2, flags);
-		printf("i want message %s\n", request);
-   	 	if (sent <= 0){
-        		printf("Error when running \"send\": %d\n errno = %d\n", 
-			sent, errno);
-        		return -1;
-    		}
+		while(0 == 0){
+                    if(lastReq == NULL){
+                        sent = send(sock2, request, nbytes2, flags);
+		        printf("i want message %s\n", request);
+   	 	        if (sent <= 0){
+        	            printf("Error when running \"send\": %d\n errno = %d\n", 
+			    sent, errno);
+        		    return -1;
+    		        }
 		
-    		caught =  recv(sock2, message2, nbytes, flags);
-    		if( caught <= 0){
-        		printf("Error when running \"recv\": %d\n errno = %s\n", 
-				caught, strerror(errno));
-        		return -1;
-    		}
-		printf("Recovered: %s\n", message2);
-		memset(message2, '\0', 99*sizeof(char));
+    		        caught = recv(sock2, message2, nbytes, flags);
+    		        if( caught <= 0){
+        	            printf("Error when running \"recv\": %d\n errno = %s\n", 
+			            caught, strerror(errno));
+        		    return -1;
+    		        }
+		    } else continue;
+                    printf("Recovered: %s\n", message2);
+		    memset(message2, '\0', 99*sizeof(char));
+                }
 		close(sock2);	
 		
 		return 0;
 }
+
+void insert(char * msg){
+    request * newMessage, tmp;
+    newMessage = (request *) malloc(sizeof(request));
+    strcpy(newMessage->thisReq, msg);
+    newMessage->nextReq = NULL;
+    if (lastReq == NULL){
+        lastReq = newMessage;
+        oldReq = newMessage;
+        newMessage->prevReq = NULL;
+        return;
+    }
+    lastReq->nextReq = newMessage;
+    newMessage->prevReq = lastReq;
+    lastReq = newMessage;
+}
+
+int length() {
+    int length = 0;
+    struct request *cur;
+    for(cur = lastReq; cur != NULL; cur = cur->prevReq) length++;
+    return length;
+}
+
+void removeTail(){
+    request * tmp;
+    tmp = oldReq;
+    oldReq = oldReq->nextReq;
+    oldReq->prevReq = NULL;
+    //printf("Removed %s\nNew tail %s\n", tmp->thisMsg, oldMessage->thisMsg);
+    free(tmp);
+}
+
